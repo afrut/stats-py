d4858b2eabb9703efd958e34cf076b3d2a64943c
eric373 <ericryanco@gmail.com>
2018-03-17 16:54:34 -0400

    Simple regression with statsmodels

--- statsmodels_ols.py
+++ statsmodels_ols.py
@@ -1,9 +1,35 @@
 import subprocess as sp
-import numpy as np
-import statsmodels.api as sm
-
 sp.call( 'cls', shell = True )
 
-spector_data = sm.datasets.spector.load()
+import numpy as np
+from statsmodels import api as sm
+import matplotlib.pyplot as plt
 
+#*********************************************************************
+#
+# DATA CREATION
+#
+#*********************************************************************
+n = 100                          # define number of samples
+x = np.linspace( 0, 10, n )      # create independent variable from 0 to 10
+X = np.column_stack( (x, x**2) ) # create two columns from the tuple of arrays
+beta = np.array( [1, 0.1, 10] )  # specify the true value of the parameters
+e = np.random.normal( size = n ) # simulate random error from a normal distribution
+# TODO: add column_stack to numpy_snippets
 
+# add constant to the independent variable matrix for regression intercept
+X = sm.add_constant ( X )
+
+# take sum-product of X matrix and simulated true parameters and add error
+y = np.dot( X, beta ) + e
+
+#*********************************************************************
+#
+# MODEL FITTING
+#
+#*********************************************************************
+model = sm.OLS( y, X )          # create the model object
+results = model.fit()           # perform ordinary least squares fitting
+print( results.summary() )
+print( 'Parameters: ', results.params )
+print( 'R2: ', results.rsquared )
