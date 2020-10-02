a80a64bc50bc59c1cb300b2637643587702a1e03
eric373 <ericryanco@gmail.com>
2020-10-02 12:57:18 -0400

    Added samples for the Geometric Distribution and Negative Binomial Distribution

--- distributions.py
+++ distributions.py
@@ -1,4 +1,10 @@
 #exec(open('distributions.py').read())
+# ----------------------------------------------------------------------
+#
+# Reference Material Montgomery & Runger: Applied statistics and Probability
+# for Engineers 7ed
+#
+# ----------------------------------------------------------------------
 import subprocess as sp
 import importlib as il
 import numpy as np
@@ -16,7 +22,9 @@ if __name__ == '__main__':
     # ----------------------------------------------------------------------
     # Plotting Parameters
     # ----------------------------------------------------------------------
-    edgecolor = np.array([0.121568627,0.466666667,0.705882353]) / 1.6
+    edgecolor = np.array([0.121568627,0.466666667,0.705882353]) / 1.6   # color of the edges of the bar graph rectangles
+    show = False                                                        # whether or not to show plots
+    close = True                                                        # whether or not to close plots
 
     # ----------------------------------------------------------------------
     #
@@ -32,9 +40,12 @@ if __name__ == '__main__':
     p = 0.1
     k = 2
     n = 18
+    print('----------------------------------------')
+    print('  Binomial Distribution')
+    print('----------------------------------------')
+    print('{0}An event E occurs with a probability of {1}.'.format(space, p))
     prob = stats.binom.pmf(k, n, p)
-    print('An event E occurs with a probability of {0}.'.format(p))
-    print('{0} The probability that E occurs {1} times'.format(space, k) +\
+    print('{0}The probability that E occurs {1} times'.format(space, k) +\
         ' in the next {0} events is {1:.4}.'.format(n, prob))
     x = list()
     probs = list()
@@ -42,15 +53,16 @@ if __name__ == '__main__':
         prob = stats.binom.pmf(k, n, p)
         x.append(k)
         probs.append(round(prob, 4))
-        print('{0} The probability that E occurs {1} times'.format(space, k) +\
-        ' in the next {0} events is {1:.4}.'.format(n, prob))
+        print('{0}The probability that E occurs {1} times'.format(space, k) +\
+            ' in the next {0} events is {1:.4}.'.format(n, prob))
 
     k = 4
     prob = 0
     for k in range(0, k):
         prob = prob + stats.binom.pmf(k, n, p)
     prob = 1 - prob
-    print('{0} The probability that E occurs >= {1} times is {2:.4}'.format(space, k + 1, prob))
+    print('{0}The probability that E occurs >= {1} times in {2} events {3:.4} is'\
+        .format(space, k + 1, n, prob))
 
     k1 = 3
     k2 = 7
@@ -59,7 +71,13 @@ if __name__ == '__main__':
         prob = prob + stats.binom.pmf(k, n, p)
     probcum = stats.binom.cdf(k2, n, p) - stats.binom.cdf(k1, n, p)\
         - stats.binom.pmf(k2, n, p) + stats.binom.pmf(k1, n, p)
-    print('{0} The probability that E occurs {1} <= k < {2} times is {3:.4}'.format(space, k1, k2, prob))
+    print('{0}The probability that E occurs {1} <= k < {2} times in {3} events is {4:.4}'\
+        .format(space, k1, k2, n, prob))
+
+    mu = stats.binom.mean(n, p)
+    sigmasq = stats.binom.var(n, p)
+    print('{0}The mean of this binomial distribution is {1:.4}'.format(space, mu))
+    print('{0}The variance of this binomial distribution is {1:.4}'.format(space, sigmasq))
 
     x = np.array(x)
     probs = np.array(probs)
@@ -67,4 +85,124 @@ if __name__ == '__main__':
         ,title = 'Binomial Distribution; p = {0:.4}, n = {1}, k = {2}'.format(p, n, k)
         ,align = 'edge'
         ,edgecolor = edgecolor
+        ,show = show, close = close)
+    print('')
+
+    # ----------------------------------------------------------------------
+    #
+    # Geometric Distribution
+    # Consider the following situation: The probability of encountering a
+    # poisonous molecule in a sample is 0.1. What is the probability of
+    # encountering the first sample with a poisonous molecule on the 10th
+    # sample? How about the 15th? The geometric distribution answers these
+    # questions. It gives the probability that the first occurrence of event E
+    # (with probability p) occurs in exactly k events.
+    #
+    # ----------------------------------------------------------------------
+    p = 0.1
+    k = 5
+    print('----------------------------------------')
+    print('  Geometric Distribution')
+    print('----------------------------------------')
+    print('{0}An event E occurs with a probability of {1}.'.format(space, p))
+    prob = stats.geom.pmf(k, p)
+    print('{0}The probability that E first occurs in {1} times is {2:.4}'\
+        .format(space, k, prob))
+    x = list()
+    probs = list()
+    ks = k + 1
+    for k in range(0, ks):
+        prob = stats.geom.pmf(k, p)
+        x.append(k)
+        probs.append(round(prob, 4))
+        print('{0}The probability that E first occurs in {1} times is {2:.4}'\
+            .format(space, k, prob))
+
+    k = 4
+    prob = 0
+    for k in range(0, k):
+        prob = prob + stats.geom.pmf(k, p)
+    prob = 1 - prob
+    print('{0}The probability that E first occurs in >= {1} events is {2:.4}'.format(space, k + 1, prob))
+
+    k1 = 3
+    k2 = 7
+    prob = 0
+    for k in range(k1, k2):
+        prob = prob + stats.geom.pmf(k, p)
+    probcum = stats.geom.cdf(k2, p) - stats.geom.cdf(k1, p)\
+        - stats.geom.pmf(k2, p) + stats.geom.pmf(k1, p)
+    print('{0}The probability that first E occurs in {1} <= k < {2} events is {3:.4}'.format(space, k1, k2, prob))
+
+    mu = stats.geom.mean(p)
+    sigmasq = stats.geom.var(p)
+    print('{0}The mean of this geomial distribution is {1:.4}'.format(space, mu))
+    print('{0}The variance of this geomial distribution is {1:.4}'.format(space, sigmasq))
+
+    x = np.array(x)
+    probs = np.array(probs)
+    plots.barplot(x, probs
+        ,title = 'Geometric Distribution; p = {0:.4}, n = {1}, k = {2}'.format(p, n, k)
+        ,align = 'edge'
+        ,edgecolor = edgecolor
+        ,show = show, close = close)
+    print('')
+
+    # ----------------------------------------------------------------------
+    #
+    # Negative Binomial Distribution
+    # This is similar to the Geometric Distribution with a slight twist.
+    #
+    # Consider the following situation: The probability of encountering a
+    # poisonous molecule in a sample is 0.1. What is the probability of
+    # encountering the exactly 3 samples with a poisonous molecule by the 10th
+    # sample? How about exactly 5 samples by the 15th? The Negative Binomial
+    # Distribution answers these questions. It gives the probability that the
+    # kth occurrence of event E (with probability p) occurs by exactly n events.
+    #
+    # ----------------------------------------------------------------------
+    p = 0.1
+    n = 10          # number of events
+    k = 4           # number of occurrences of event E
+    pplot = p
+    nplot = n
+    kplot = k
+    print('----------------------------------------')
+    print('  Negative Binomial Distribution')
+    print('----------------------------------------')
+    print('{0}An event E occurs with a probability of {1}.'.format(space, p))
+    # pmf(num events - num event E, num event E, probability)
+    prob = stats.nbinom.pmf(n - k, k, p)
+    print('{0}The probability that E occurs exactly {1} times by the {2}th event is {3:.4}'\
+        .format(space, k, n, prob))
+    x = list()
+    probs = list()
+    for k in range(1, n + 1):
+        prob = stats.nbinom.pmf(n - k, k, p)
+        x.append(k)
+        probs.append(round(prob, 4))
+        print('{0}The probability that E occurs exactly {1} times by the {2}th event is {3:.4}'\
+            .format(space, k, n, prob))
+
+    n = 5
+    k = 3
+    p  = 0.2
+    print('')
+    print('{0}An event E occurs with a probability of {1}.'.format(space, p))
+    prob = 0
+    ns = range(k, n + 1)
+    for n in ns:
+        prob = prob + stats.nbinom.pmf(n - k, k, p)
+    print('{0}The probability that E occurs exactly {1} times by the <= {2}th event is {3:.4}'\
+        .format(space, k, n, prob))
+
+    x = np.array(x)
+    probs = np.array(probs)
+    plots.barplot(x, probs
+        ,title = 'Negative Binomial Distribution; p = {0:.4}, n = {1}, k = {2}'\
+            .format(pplot, nplot, kplot)
+        ,align = 'edge'
+        ,edgecolor = edgecolor
         ,show = True, close = True)
+    print('')
+
