5fc890109486525be2ef670bf77b97de69d84f83
eric373 <ericryanco@gmail.com>
2020-10-07 13:03:05 -0400

    Added Hypergeomeric and Poisson Distributions

--- distributions.py
+++ distributions.py
@@ -13,6 +13,7 @@ import plots
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import counting_techniques as ct
+import math
 
 if __name__ == '__main__':
     sp.call('cls', shell = True)
@@ -185,6 +186,7 @@ if __name__ == '__main__':
     assert(round(sigmasq - ((1 - p) / p**2), 8) == 0)
     print('{0}The mean of this geometric distribution is {1:.8}'.format(space, mu))
     print('{0}The variance of this geometric distribution is {1:.8}'.format(space, sigmasq))
+    print('')
 
     # ----------------------------------------------------------------------
     #
@@ -264,6 +266,172 @@ if __name__ == '__main__':
     assert(round(sigmasq - (r * (1 - p) / p**2), 8) == 0)
     print('{0}The mean of this negative binomial distribution is {1:.8}'.format(space, mu))
     print('{0}The variance of this negative binomial distribution is {1:.8}'.format(space, sigmasq))
+    print('')
+
+    # ----------------------------------------------------------------------
+    #
+    # Hypergeometric Distribution
+    # Consider the following situation: 850 parts produced contains 50
+    # defective parts. Two parts are selected at random without replacement.
+    # What is the probability that both parts are defective? What is the
+    # probability that both parts are not defective? What is the probability
+    # that only 1 of the parts is defective?
+    #
+    # Suppose that the event of interest is a part being defective. The
+    # Hypergeometric Distribution gives the probability of the sample
+    # containing x = 2 defective parts. In general, consider a pool of N = 850
+    # objects, K = 50 of which are of interest. A sample of size n = 2 is drawn
+    # from this pool. The Hypergeometric Distribution gives the probability
+    # that x = 1 of these objects is of interest.
+    #
+    # ----------------------------------------------------------------------
+    print('----------------------------------------')
+    print('  Hypergeometric Distribution')
+    print('----------------------------------------')
+    # ----------------------------------------
+    # probability mass function
+    # ----------------------------------------
+    N = 850     # number of total objects
+    K = 50      # number of objects of interest
+    n = 2       # number of objects drawn from pool as a sample
+    x = 1       # number of objects of interest in the sample
+    prob = stats.hypergeom.pmf(x, N, K, n)
+    prob2 = ct.comb(K, x) * ct.comb(N - K, n - x) / ct.comb(N, n)
+    assert(round(prob - prob2, 8) == 0)
+    assert(0.111 - round(prob,3) == 0)
+    print('{0}A pool contains N = {1} objects.'.format(space, N))
+    print('{0}K = {1} of these objects are of interest.'.format(space, K))
+    print('{0}A sample of size n = {1} is drawn from the pool.'.format(space, n))
+    print('{0}The probability that the pool contains x = {1} objects of interest is {2:.8}.'\
+        .format(space, x, prob))
+    probs = list()
+    xs = range(0, n + 1)
+    for x in xs:
+        prob = stats.hypergeom.pmf(x, N, K, n)
+        probs.append(round(prob, 8))
+        print('{0}The probability that the sample contains x = {1} objects of interest is {2:.8}.'\
+            .format(space, x, prob))
+
+    # ----------------------------------------
+    # plotting
+    # ----------------------------------------
+    xs = np.array(xs)
+    probs = np.array(probs)
+    plots.barplot(xs, probs
+        ,title = 'Hypergeometric Distribution; N = {0}, K = {1}, n = {2}'.format(N, K, n)
+        ,align = 'edge'
+        ,edgecolor = edgecolor
+        ,show = False, close = False)
+    print('')
+
+    # ----------------------------------------
+    # cumulative probabilities
+    # ----------------------------------------
+    N = 300
+    K = 100
+    n = 4
+    x = 2
+    probcum = stats.hypergeom.cdf(x, N, K, n)
+    probcum = 1 - probcum + stats.hypergeom.pmf(x, N, K, n)
+    prob = 0
+    xs = range(0, x)
+    for xl in xs:
+        prob = prob + stats.hypergeom.pmf(xl, N, K, n)
+    prob = 1 - prob
+    assert(round(probcum - prob, 8) == 0)
+    assert(0.407 - round(probcum, 3) == 0)
+    print('{0}The probability that the sample of size n = {1} contains'.format(space, n) +
+        ' x >= {0} objects of interest is {1:.8}'.format(x, probcum))
+
+    # ----------------------------------------
+    # mean and variance
+    # ----------------------------------------
+    mu = stats.hypergeom.mean(N, K, n)
+    sigmasq = stats.hypergeom.var(N, K, n)
+    p = K / N
+    assert(round(mu - (n * p), 8) == 0)
+    assert(round(sigmasq - (n * p * (1 - p) * ((N - n)/(N - 1))), 8) == 0)
+    print('{0}The mean of this hypergeometric distribution is {1:.8}'.format(space, mu))
+    print('{0}The variance of this hypergeometric distribution is {1:.8}'.format(space, sigmasq))
+    print('')
+
+    # ----------------------------------------------------------------------
+    #
+    # Poisson Distribution
+    # Consider the following situation: Flaws occur at random along the length
+    # of a thin copper wire. It is given that the average number of flaws per
+    # mm = lambda. What is the probability of encounter x = 10 flaws
+    # in T = 5 mm of wire? The Poisson Distribution answers these kinds of
+    # questions.
+    #
+    # In general, given an average number of occurrences of event E per unit
+    # parameter(lambda), the Poisson Distribution gives the probability of
+    # event E occurring x times in T units.
+    #
+    # ----------------------------------------------------------------------
+    print('----------------------------------------')
+    print('  Poisson Distribution')
+    print('----------------------------------------')
+    # ----------------------------------------
+    # probability mass function
+    # ----------------------------------------
+    lamb = 2.3      # number of times event E occurs per unit on average
+    x = 10          # number of times event E occurs
+    T = 5           # unit examined
+    prob = stats.poisson.pmf(x, lamb * T)
+    prob2 = math.exp(-lamb * T) * (lamb * T)**x / math.factorial(x)
+    assert(round(prob - prob2, 8) == 0)
+    assert(0.113 - round(prob, 3) == 0)
+    print('{0}An event E occurs {1} times per unit on average'.format(space, lamb))
+    print('{0}The number of units examined T = {1}'.format(space, T))
+    print('{0}The probability of E occurring x = {1} times is {2:.8}'\
+        .format(space, x, prob))
+    probs = list()
+    xs = range(0, x + 5)
+    for xl in xs:
+        prob = stats.poisson.pmf(xl, lamb * T)
+        probs.append(prob)
+        print('{0}The probability that E occurs {1} times'.format(space, xl) +\
+            ' in T = {0} units is {1:.8}'.format(T, prob))
+    
+    # ----------------------------------------
+    # plotting
+    # ----------------------------------------
+    xs = np.array(xs)
+    probs = np.array(probs)
+    plots.barplot(xs, probs
+        ,title = 'Poisson Distribution; lamb = {0}, T = {1}'.format(lamb, T)
+        ,align = 'edge'
+        ,edgecolor = edgecolor
+        ,show = False, close = False)
+    print('')
+
+    # ----------------------------------------
+    # cumulative probabilities
+    # ----------------------------------------
+    x = 1
+    T = 2
+    probcum = stats.poisson.cdf(x, lamb * T)
+    probcum = 1 - probcum + stats.poisson.pmf(x, lamb * T)
+    prob = 0
+    for xl in range(0, x):
+        prob = prob + stats.poisson.pmf(xl, lamb * T)
+    prob = 1 - prob
+    assert(round(prob - probcum, 8) == 0)
+    assert(0.9899 - round(probcum, 4) == 0)
+    print('{0}The probability that E occurs x >= {1} times P(x >= {2}) = {3:.8}'\
+        .format(space, x, x, probcum))
+
+    # ----------------------------------------
+    # mean and variance
+    # ----------------------------------------
+    mu = stats.poisson.mean(lamb * T)
+    sigmasq = stats.poisson.var(lamb * T)
+    assert(round(mu - (lamb * T), 8) == 0)
+    assert(round(sigmasq - (lamb * T), 8) == 0)
+    print('{0}The mean of this poisson distribution is {1:.8}'.format(space, mu))
+    print('{0}The variance of this poisson distribution is {1:.8}'.format(space, sigmasq))
+
 
     # show plots
     if(show):
