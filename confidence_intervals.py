561fbfd97532879da67a9ef77b425fa3e2114efb
eric373 <ericryanco@gmail.com>
2020-10-30 12:10:48 -0400

    Added notes, plots and comments for confidence intervals

--- confidence_intervals.py
+++ confidence_intervals.py
@@ -3,20 +3,26 @@ import subprocess as sp
 import numpy as np
 import pandas as pd
 import importlib as il
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import math
+import scipy.stats as stats
 import plots
+
+def pdfnorm(x, mu, sigma):
+    return 1 / (math.sqrt(2 * math.pi) * sigma) * np.exp(-((x - mu)**2) / (2 * sigma**2))
 
 if __name__ == '__main__':
     sp.call('cls', shell = True)
     il.reload(plots)
-    plt.close()
+    plt.close('all')
 
     # ----------------------------------------------------------------------
-    # Create a population distribution for experimental purposes.
+    # Create a population of 1 million heights in cm with known mean and
+    # variance. This data set will be used for simulations.
     # ----------------------------------------------------------------------
     mu = 175
-    sigma = 15
+    sigma = 5
     population = np.random.randn(1000000) * sigma + mu;
     mu = np.mean(population)                # population mean
     sigma = np.std(population, ddof = 0)    # population standard deviation
@@ -27,109 +33,186 @@ if __name__ == '__main__':
            str(round(min(population), 2)) + ',' +
            str(round(max(population), 2)) + ']')
     print('')
-    dfPopulation = pd.DataFrame(population, columns = ['height'])
+    dfPop = pd.DataFrame(population, columns = ['height'])
 
     # ----------------------------------------------------------------------
-    # Draw a sample from from the population with sample size of n.
+    # Create the sampling distribution
     # ----------------------------------------------------------------------
-    n = 20
-    sample = np.random.choice(population, size = n)
-    dfSample = pd.DataFrame(sample, columns = ['height'])
-    xbar = round(np.mean(sample), 2)        # sample mean
-    sx = round(np.std(sample, ddof = 0), 2) # sample standard deviation
-    print('Single Sample:')
-    print('Sample mean = ' + str(round(xbar, 2)))
-    print('Sample standard deviation = ' + str(round(sx, 2)))
-    print('')
-
-    # create the sampling distribution
     lsx = list()
+    n = 20
     for cnt in range(0, 100000):
         samp = np.random.choice(population, size = n)
         lsx.append(np.mean(samp))
-    dfSamplingDist = pd.DataFrame(lsx, columns = ['height'])
+    dfSampDist = pd.DataFrame(lsx, columns = ['height'])
     x = np.array(lsx)
     x.sort()
-    sampmu = x.mean()
-    sampsigma = sigma / math.sqrt(n)
+    sampDistMu = x.mean()
+    sampDistSigma = sigma / math.sqrt(n)
     ex = round(x.mean(), 2)
     sex = round(np.std(x), 2)
     print('Sampling Distribution:')
-    print('E(xbar) = {0:.8}'.format(ex))
-    print('SE(xbar) = {0:.8}'.format(sex))
+    print('E(sampMean) = {0:.8}'.format(ex))
+    print('SE(sampMean) = {0:.8}'.format(sex))
     print('')
 
-    # ----------------------------------------------------------------------
-    # Plot a histogram of the sampling distribution and the probability density
-    # function for the corrensponding normal distribution. Compare the shape
-    # of the histogram and the corresponding normal distribution with variance
-    # sigma / sqrt(n). Note that their shapes are identical.
-    # ----------------------------------------------------------------------
-    nrow = 2
+    # plotting parameters
+    nrow = 1
     ncol = 1
     nplot = 1
     fig = plt.figure(figsize = (14.4, 9))
-    ax = fig.add_subplot(nrow, ncol, nplot)
-    plots.histogram(dfSamplingDist
-        ,fig = fig
-        ,ax = ax
-        ,numBins = 20
-        ,title = 'Histogram vs Normal Distribution'
-        ,xlabel = ['height']
-        ,ylabel = ['count'])
-    nplot = nplot + 1
-
-    # normal distribution centered around sample distribution mean with variance
-    # sigma / sqrt(n)
-    fx = 1 / (math.sqrt(2 * math.pi) * sampsigma) * np.exp(-((x - sampmu)**2) / (2 * sampsigma**2))
-    plots.scatter(x
-        ,fx
-        ,fig = fig
-        ,ylim = (fx.min(), fx.max())
-        ,title = ''
-        ,axesNew = True
-        ,markersize = 0
-        ,linewidth = 2
-        ,color = (1, 0.4, 0.4, 1))
 
     # ----------------------------------------------------------------------
     # Plot the corrensponding normal distributions of the population and sampling
-    # distributions. Note that the sampling distribution is much "tighter" implying
+    # distributions. NOTE: The sampling distribution is much "tighter" implying
     # a smaller variance. Note that both are centered around the same value.
     # ----------------------------------------------------------------------
-    ax = fig.add_subplot(nrow, ncol, nplot)
-    plots.scatter(x
-        ,fx
-        ,fig = fig
-        ,ax = ax
-        ,ylim = (fx.min(), fx.max())
-        ,title = ''
-        ,markersize = 0
-        ,linewidth = 2
-        ,color = (1, 0.4, 0.4, 1))
-    x = dfPopulation.loc[:, 'height'].values
+    # get min/max values of population and create an array of x-values
+    x = dfPop.loc[:, 'height'].values
     xmin = x.min()
     xmax = x.max()
-    x = np.linspace(xmin, xmax, 100)
-    fx = 1 / (math.sqrt(2 * math.pi) * sigma) * np.exp(-((x - mu)**2) / (2 * sigma**2))
-    ylim = ax.get_ylim()
-    if fx.max() > ylim[1]:
-        ylim = (ylim[0], ylim[1])
+    x = np.linspace(xmin, xmax, 500)
+
+    # calculate normal probability density function of sampling distribution
+    # by using sigma/sqrt(n) as the sampling distribution variance
+    ysamp = pdfnorm(x, sampDistMu, sampDistSigma)
+    ylim = (0, ysamp.max())
+
+    # plot sampling distribution 
+    ax = fig.add_subplot(nrow, ncol, nplot)
     plots.scatter(x
-        ,fx
+        ,ysamp
         ,fig = fig
         ,ax = ax
         ,ylim = ylim
         ,title = ''
         ,markersize = 0
         ,linewidth = 2
-        ,color = (0.4, 0.4, 1, 1))
+        ,color = plots.RED)
+
+    # calculate normal probability density function of population with population variance
+    ypop = pdfnorm(x, mu, sigma)
+
+    # get the largest y value for y axis limits
+    ylim = ax.get_ylim()
+    if ypop.max() > ylim[1]:
+        ylim = (ylim[0], ylim[1])
+
+    # plot the population distribution
+    plots.scatter(x
+        ,ypop
+        ,fig = fig
+        ,ax = ax
+        ,ylim = ylim
+        ,title = ''
+        ,markersize = 0
+        ,linewidth = 2
+        ,color = plots.BLUE)
     nplot = nplot + 1
-    ax.legend(['Sampling Distribution','Population Distribution'])
+
+    # ----------------------------------------------------------------------
+    # Plot areas that represent a 95% confidence interval. The population variance
+    # sigma is assumed to be known. NOTE: The shaded region is represents a
+    # range of x values that represent a 1 - alpha probability with respect
+    # to the sampling distribution.
+    # ----------------------------------------------------------------------
+    alpha = 0.05
+
+    # calculate the upper and lower z values that represent the standardized
+    # upper and lower limits of the confidence interval
+    zlo = stats.norm.ppf(alpha / 2)
+    zhi = stats.norm.ppf(1 - (alpha / 2))
+
+    # calculate the upper and lower x values that represent the
+    # upper and lower limits of the confidence interval
+    xlo = zlo * sigma / math.sqrt(n) + sampDistMu
+    xhi = zhi * sigma / math.sqrt(n) + sampDistMu
+
+    # create an array of x values that define the fill region
+    xfill = np.linspace(xlo, xhi, 500)
+    yfill = pdfnorm(xfill, sampDistMu, sampDistSigma)
+    ax.fill_between(xfill, yfill, color = plots.RED)
+
+    # plot the mean of the sampling distribution
+    ax.plot(np.array([sampDistMu, sampDistMu])
+        ,np.array((ylim))
+        ,linewidth = 1.25
+        ,color = plots.LIGHT_RED
+        ,linestyle = 'dashed')
+
+    # ----------------------------------------------------------------------
+    # Build multiple confidence intervals by repeated sampling. Visualize.
+    # NOTE: Not all confidence intervals contain the true mean mu.
+    # ----------------------------------------------------------------------
+    nDraw = 100     # number of samples to draw
+    n = 20          # sample size
+
+    # Create an array of y positions where the confidence intervals are to be drawn.
+    # Exclude that first and last points so as not to plot on the edges of the plotting area.
+    ypos = np.linspace(ylim[0], ylim[1], nDraw + 2)
+    ypos = ypos[1:-1]
+
+    # Visualization parameters
+    whiskerWidth = 1e-3 / 2
+
+    cntNoMean = 0
+    for y in ypos:
+        markersize = 3
+        linewidth = 0.75
+        color = plots.GREEN
+
+        # draw a sample
+        sample = np.random.choice(population, size = n)
+        sampMean = sample.mean()
+
+        # calculate the confidence interval
+        cilo = zlo * sigma / math.sqrt(n) + sampMean
+        cihi = zhi * sigma / math.sqrt(n) + sampMean
+
+        # count confidence intervals that do not contain
+        # the sampling distribution mean
+        if sampDistMu < cilo or sampDistMu > cihi:
+            cntNoMean = cntNoMean + 1
+            markersize = 7
+            linewidth = 2
+            color = plots.ORANGE
+
+        # visualize
+        ax.plot(sampMean
+            ,y
+            ,marker = 'o'
+            ,markersize = markersize
+            ,linewidth = 0
+            ,color = color)
+        ax.plot([cilo, cihi]
+            ,[y, y]
+            ,linewidth = linewidth
+            ,color = color)
+        ax.plot([cilo, cilo]
+            ,[y - whiskerWidth, y + whiskerWidth]
+            ,linewidth = linewidth
+            ,color = color)
+        ax.plot([cihi, cihi]
+            ,[y - whiskerWidth, y + whiskerWidth]
+            ,linewidth = linewidth
+            ,color = color)
+
+    print('Out of {0} samples, {1} confidence intervals do not contain the sampling distribution mean.'\
+        .format(nDraw, cntNoMean))
+    print('')
+
+    # format plot
+    legend =\
+    [
+         mpl.lines.Line2D([0], [0], color = plots.RED, linewidth = 2, label = 'Sampling Distribution')
+        ,mpl.lines.Line2D([0], [0], color = plots.BLUE, linewidth = 2, label = 'Population Distribution')
+        ,mpl.lines.Line2D([0], [0], color = plots.LIGHT_RED, linewidth = 1.25, linestyle = 'dashed', label = 'Sampling Distribution Mean')
+        ,mpl.patches.Patch(facecolor = plots.RED, label = '{0}% Probability Interval'.format(int((1 - alpha) * 100)))
+        ,mpl.lines.Line2D([0], [0], color = plots.GREEN, linewidth = 1, label = '{0}% Confidence Interval'.format(int((1 - alpha) * 100)))
+        ,mpl.lines.Line2D([0], [0], color = plots.ORANGE, linewidth = 2, label = 'Confidence Intervals that do not bound the sampling distribution mean')
+    ]
+    ax.legend(handles = legend)
     ax.set_xlabel('height')
     ax.set_ylabel('f(x)')
-
-    # TODO: code based on quickstart.ipynb
 
 
 
