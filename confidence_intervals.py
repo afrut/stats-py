9ba20afe7ab19f2987cde0a2267cae644dd7ff48
eric373 <ericryanco@gmail.com>
2020-11-02 23:27:19 -0500

    Added content for confidence intervals

--- confidence_intervals.py
+++ confidence_intervals.py
@@ -55,11 +55,7 @@ if __name__ == '__main__':
     print('SE(sampMean) = {0:.8}'.format(sex))
     print('')
 
-    # plotting parameters
-    nrow = 1
-    ncol = 1
-    nplot = 1
-    fig = plt.figure(figsize = (14.4, 9))
+    figsize = (14.4, 9)
 
     # ----------------------------------------------------------------------
     # Plot the corrensponding normal distributions of the population and sampling
@@ -78,7 +74,8 @@ if __name__ == '__main__':
     ylim = (0, ysamp.max())
 
     # plot sampling distribution 
-    ax = fig.add_subplot(nrow, ncol, nplot)
+    fig = plt.figure(figsize = figsize)
+    ax = fig.add_subplot(1, 1, 1)
     plots.scatter(x
         ,ysamp
         ,fig = fig
@@ -107,7 +104,6 @@ if __name__ == '__main__':
         ,markersize = 0
         ,linewidth = 2
         ,color = plots.BLUE)
-    nplot = nplot + 1
 
     # ----------------------------------------------------------------------
     # Plot areas that represent a 95% confidence interval. The population variance
@@ -196,7 +192,7 @@ if __name__ == '__main__':
             ,linewidth = linewidth
             ,color = color)
 
-    print('Out of {0} samples, {1} confidence intervals do not contain the sampling distribution mean.'\
+    print('Out of {0} samples, {1} standard normal confidence intervals do not contain the sampling distribution mean.'\
         .format(nDraw, cntNoMean))
     print('')
 
@@ -213,10 +209,135 @@ if __name__ == '__main__':
     ax.legend(handles = legend)
     ax.set_xlabel('height')
     ax.set_ylabel('f(x)')
-
-
-
-
-
+    ax.set_title('{0} Confidence Intervals using a Standard Normal Distribution'.format(nDraw))
     fig.tight_layout()
+
+    # ----------------------------------------------------------------------
+    # Student's t Distribution as Compared to the Standard Normal Distribution
+    # ----------------------------------------------------------------------
+    # Calculate probability density function values for the standard normal distribution
+    yz = stats.norm.pdf(x, loc = mu, scale = sigma)
+
+    # Draw a sample and caluculate the sample standard deviation.
+    sample = np.random.choice(population, size = n)
+    s = sample.std(ddof = 1)
+
+    # Calculate the probability density function values for the t distribution.
+    # df = n - 1 specifies to use n - 1 degrees of freedom
+    yt = stats.t.pdf(x, df = n - 1, loc = mu, scale = s)
+
+    # Visualize Student's t Distribution and compare to the standard normal distribution
+    ylim = (0, max(yz.max(), yt.max()))
+    fig, ax = plots.scatter(x, yz
+        ,figsize = figsize
+        ,ylim = ylim
+        ,xlabel = 'height'
+        ,ylabel = 'f(x)'
+        ,linewidth = 2
+        ,markersize = 0
+        ,color = plots.BLUE)
+    plots.scatter(x, yt
+        ,fig = fig
+        ,ax = ax
+        ,ylim = ylim
+        ,title = ''
+        ,linewidth = 2
+        ,markersize = 0
+        ,color = plots.RED)
+
+    # ----------------------------------------------------------------------
+    # Build multiple confidence intervals using the t distribution.
+    # ----------------------------------------------------------------------
+    # calculate the upper and lower t values that represent the
+    # upper and lower limits of the confidence interval using the t distribution
+    tlo = stats.t.ppf(alpha / 2, df = n - 1)
+    thi = stats.t.ppf(1 - (alpha / 2), df = n - 1)
+
+    # upper and lower limits of the confidence interval
+    xlo = tlo * sigma / math.sqrt(n) + sampDistMu
+    xhi = thi * sigma / math.sqrt(n) + sampDistMu
+
+    # create an array of x values that define the fill region
+    xfill = np.linspace(xlo, xhi, 500)
+    yfill = stats.t.pdf(xfill, n - 1, loc = sampDistMu, scale = s)
+    ax.fill_between(xfill, yfill, color = plots.RED)
+
+    # plot the mean of the sampling distribution
+    ax.plot(np.array([sampDistMu, sampDistMu])
+        ,np.array((ylim))
+        ,linewidth = 1.25
+        ,color = plots.LIGHT_RED
+        ,linestyle = 'dashed')
+
+    # Create an array of y positions where the confidence intervals are to be drawn.
+    # Exclude that first and last points so as not to plot on the edges of the plotting area.
+    ypos = np.linspace(ylim[0], ylim[1], nDraw + 2)
+    ypos = ypos[1:-1]
+
+    # Visualization parameters
+    whiskerWidth = 1e-3 / 2
+
+    cntNoMean = 0
+    for y in ypos:
+        markersize = 3
+        linewidth = 0.75
+        color = plots.GREEN
+
+        # draw a sample
+        sample = np.random.choice(population, size = n)
+        sampMean = sample.mean()
+
+        # calculate the confidence interval
+        cilo = tlo * sigma / math.sqrt(n) + sampMean
+        cihi = thi * sigma / math.sqrt(n) + sampMean
+
+        # count confidence intervals that do not contain
+        # the sampling distribution mean
+        if sampDistMu < cilo or sampDistMu > cihi:
+            cntNoMean = cntNoMean + 1
+            markersize = 7
+            linewidth = 2
+            color = plots.ORANGE
+
+        # visualize
+        ax.plot(sampMean
+            ,y
+            ,marker = 'o'
+            ,markersize = markersize
+            ,linewidth = 0
+            ,color = color)
+        ax.plot([cilo, cihi]
+            ,[y, y]
+            ,linewidth = linewidth
+            ,color = color)
+        ax.plot([cilo, cilo]
+            ,[y - whiskerWidth, y + whiskerWidth]
+            ,linewidth = linewidth
+            ,color = color)
+        ax.plot([cihi, cihi]
+            ,[y - whiskerWidth, y + whiskerWidth]
+            ,linewidth = linewidth
+            ,color = color)
+
+    print('Out of {0} samples, {1} t distribution confidence intervals do not contain the sampling distribution mean.'\
+        .format(nDraw, cntNoMean))
+    print('')
+
+    legend =\
+    [
+         mpl.lines.Line2D([0], [0], color = plots.BLUE, linewidth = 2, label = 'Standard Normal')
+        ,mpl.lines.Line2D([0], [0], color = plots.RED, linewidth = 2, label = 't Distribution')
+        ,mpl.lines.Line2D([0], [0], color = plots.LIGHT_RED, linewidth = 1.25, linestyle = 'dashed', label = 'Sampling Distribution Mean')
+        ,mpl.patches.Patch(facecolor = plots.RED, label = '{0}% Probability Interval'.format(int((1 - alpha) * 100)))
+        ,mpl.lines.Line2D([0], [0], color = plots.GREEN, linewidth = 1, label = '{0}% Confidence Interval'.format(int((1 - alpha) * 100)))
+        ,mpl.lines.Line2D([0], [0], color = plots.ORANGE, linewidth = 2, label = 'Confidence Intervals that do not bound the sampling distribution mean')
+    ]
+    ax.legend(handles = legend)
+    ax.set_title('{0} Confidence Intervals using a t Distribution'.format(nDraw))
+    fig.tight_layout()
+
+
+
+
+
     plt.show()
